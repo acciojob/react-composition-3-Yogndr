4cb5b932a8b159cf1f45d79bdf938244f3bc4a7a
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-08-07 00:00:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,16 @@ import Tooltip from "./Tooltip";
 const App = () => {
   return (
     <div>
+      <h2 className="tooltip">
+        <div>
       <Tooltip text="This is a tooltip" children="Hover over me"/>
+      </div>
+      </h2>
+      <p className="tooltip">
+        <div>
       <Tooltip text="This is another tooltip" children="Hover over me to see another tooltip"/>
-        
+      </div>
+        </p>
     </div>
   )
 }
