841111170d90f0179aec3f3806511887784e15d8
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-08-06 21:20:12 +0530

    Create Tooltip.jsx

---
+++ src/components/Tooltip.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+function Tooltip({text,children}) {
+ 
+    const[isVisible,setIsVisible]=useState(false)
+
+    const handleMouseEnter=()=>{
+        setIsVisible(true);
+    }
+
+    const handleMouseLeave=()=>{
+        setIsVisible(false);
+    }
+
+  return (
+    <div>
+        
+    <div className='tooltip' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} style={{margin:"200px 600px"}}>
+        {isVisible && <div className="tooltiptext" style={{color: "white", border: "1px solid black", backgroundColor:"red", width:"100px", height:"50px", borderRadius:"5px"}}>{text}</div>}
+        {children}
+        <hr style={{width:50, marginLeft:0}}>
+        </hr>
+        
+    </div>
+</div>
+  )
+}
+
+export default Tooltip
