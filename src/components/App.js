319ff53370aaa5885ef2cd0e1888cb628ab3f063
Yogndr <106022206+Yogndr@users.noreply.github.com>
2025-08-06 21:19:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Tooltip text="This is a tooltip" children="Hover over me"/>
+      <Tooltip text="This is another tooltip" children="Hover over me to see another tooltip"/>
+        
     </div>
   )
 }
